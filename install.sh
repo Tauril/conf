4324c5b86ec0858c04e7f8339265075591b005e2
Guillaume Marques <marques.guillaume@hotmail.fr>
2018-03-29 21:35:57 +0100

    install: remove useless setxkbmap call

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ git submodule update
 
 cp robbyrussell.zsh-theme .oh-my-zsh/themes/
 cp .oh-my-zsh/templates/zshrc.zsh-template .zshrc
-echo "setxkbmap us -option 'caps:swapescape' -variant altgr-intl" >> .zshrc
-echo "setxkbmap fr" >> .zshrc
+#echo "setxkbmap us -option 'caps:swapescape' -variant altgr-intl" >> .zshrc
+echo "setxkbmap fr -option 'caps:swapescape" >> .zshrc
 echo "xrdb -load .Xdefaults" >> .zshrc
 echo "source .xinitrc" >> .zshrc
 echo "xset r rate 300 50" >> .zshrc
